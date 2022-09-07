11d7b72657d0b7dbfe7551fa560981a3a89784bb
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 12:15:45 +0530

    reverse number

---
+++ ReverseNumber.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Logicalprogram
+{
+    internal class ReverseNumber
+    {
+        public static void ReverseNo()
+        {
+            Console.WriteLine("enter the no which you want revese");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int n1 = 0;
+            while (number > 0)
+            {
+                int remainder = number % 10;
+                n1 = (n1 * 10) + remainder;
+                number = number / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", n1);
+        }
+    }
+}
