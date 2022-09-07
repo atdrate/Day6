00954bd54fbcca7c861b771f34ed67de45a78faa
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 12:09:25 +0530

    fibbonacci series

---
+++ Fibonaciseries.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Logicalprogram
+{
+    internal class Fibonaciseries
+    {
+        
+        
+            public static void FibSeries()
+            {
+                int a = 0, b = 1;
+
+
+                Console.WriteLine("enter the number for limit of series");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int c;
+                for (int i = 1; i < n; i++)
+                {
+                    c = a + b;
+                    Console.WriteLine(+c);
+                    a = b;
+                    b = c;
+
+
+                }
+
+
+            }
+
+        }
+    }
