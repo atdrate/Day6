9dff47d33cc19d43f8bf0ef532c22e2a26713c36
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 12:13:13 +0530

    perfect number

---
+++ PerfectNumber.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Logicalprogram
+{
+    internal class PerfectNumber
+    {
+        public static void perfectno()
+        {
+            Console.WriteLine("enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int s = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    s = s + i;
+                }
+            }
+            if (s == n)
+            {
+                Console.WriteLine("perfect number");
+            }
+            else
+            {
+                Console.WriteLine("not a perfect number");
+            }
+        }
+    }
+}
